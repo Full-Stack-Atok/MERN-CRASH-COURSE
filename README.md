91a0f3d2053625367b6ece840e9e7364c330f7f3
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-26 20:55:24 +0800

    Another 1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MERN-CRASH-COURSE
 Learning MongoDB Express React Node
+
+# MongoDB User & Password
+komsaykerby - amVEXHHscOKP2Zmn
+
+# Testing the backend without Frontend
+POSTMAN APP
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
-const app = express()
+import productRoutes from "./routes/product.route.js";
 
-app.listen(5000, () => {
-  console.log('Server started at http://localhost:5000')
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allows JSON data in the req.body
+
+app.use("/api/products", productRoutes)
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log('Server started at http://localhost:'+ PORT);
 })

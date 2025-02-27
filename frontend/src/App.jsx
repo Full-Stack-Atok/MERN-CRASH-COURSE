620784aa681d79d6423ec2340cde34c7bdfff83b
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-27 21:56:36 +0800

    Day of building MERN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { Box } from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
+import { Box } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import Navbar from './components/navbar'
 
 function App() {
-
   return (
-    <Box minH={"100vh"}>
-      {/* {Navbar} */}
+    <Box minH={'100vh'}>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/create" element={<CreatePage/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
       </Routes>
     </Box>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,37 @@
+import { Container, Flex, Text, HStack, Button } from '@chakra-ui/react'
+import { BsPlusSquare } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <Container maxW={'1140px'} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text
+          bgGradient="linear(to-r, #00FFFF, #007BFF)"
+          bgClip='text'
+          fontSize={{ base: '22px', sm: '28px' }}
+          fontWeight='extrabold'
+        >
+          <Link to={'/'}>Product Store 🛒</Link>
+        </Text>
+
+        <HStack spacing='2' alignItems={'center'}>
+          <Link to={'/create'}>
+            <Button>
+              <BsPlusSquare></BsPlusSquare>
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 export default navbar

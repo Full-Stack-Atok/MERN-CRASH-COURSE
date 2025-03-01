d1d5b35c717dd08d3f9cb3c49188e1321a538d88
Full-Stack-Atok <komsaykerby@gmail.com>
2025-03-01 15:11:08 +0800

    Design the Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,47 +5,47 @@ import {
   HStack,
   Button,
   useColorMode,
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-
-import { PlusSquareIcon } from '@chakra-ui/icons'
-import { IoMoon } from 'react-icons/io5'
-import { IoSun } from 'react-icons/lu'
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { IoMoon, IoSunny } from 'react-icons/io5';
 
 const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Container maxW={'1140px'} px={4}>
+    <Container maxW="1140px" px={4}>
       <Flex
         h={16}
-        alignItems={'center'}
-        justifyContent={'space-between'}
+        alignItems="center"
+        justifyContent="space-between"
         flexDir={{
           base: 'column',
           sm: 'row',
         }}
       >
         <Text
-          bgGradient='linear(to-r, #00FFFF, #007BFF)'
-          bgClip='text'
+          bgGradient="linear(to-r, #00FFFF, #007BFF)"
+          bgClip="text"
           fontSize={{ base: '22px', sm: '28px' }}
-          fontWeight='extrabold'
+          fontWeight="extrabold"
         >
-          <Link to={'/'}>Product Store 🛒</Link>
+          <Link to="/">Product Store 🛒</Link>
         </Text>
-        <HStack spacing='2' alignItems={'center'}>
-          <Link to={'/create'}>
+
+        <HStack spacing={2} alignItems="center">
+          <Link to="/create">
             <Button>
-              <PlusSquareIcon fontSize={20}></PlusSquareIcon>
+              <PlusSquareIcon fontSize={20} />
             </Button>
           </Link>
           <Button onClick={toggleColorMode}>
-            {colorMode === 'light' ? <IoMoon /> : <IoSun size='20' />}
+            {colorMode === 'light' ? <IoMoon size={20} /> : <IoSunny size={20} />}
           </Button>
         </HStack>
       </Flex>
     </Container>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;

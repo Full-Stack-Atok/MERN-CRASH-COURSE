60c6dddbf55e7b3ff51128fc6feea055bc8c8df2
Full-Stack-Atok <komsaykerby@gmail.com>
2025-03-02 19:05:43 +0800

    Finished

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -38,7 +38,7 @@ export const deleteProduct = async (req, res) => {
   
   try {
     await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product delete." });
+    res.status(200).json({ success: true, message: "Product deleted." });
   } catch (error) {
     console.log("Error in deleting product:", error.message);
     res.status(500).json({ success: false, message: "Server Error!"});
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,40 @@
-import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.route.js";
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+import productRoutes from './routes/product.route.js'
+import path from 'path'
 
-dotenv.config();
+dotenv.config()
+const __dirname = path.resolve()
+const app = express()
+
+console.log('MONGO_URI:', process.env.MONGO_URI)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 // ✅ Connect to MongoDB BEFORE starting the server
-connectDB().then(() => {
-  const app = express();
-  const PORT = process.env.PORT || 5000;
+connectDB()
+  .then(() => {
+    
+    const PORT = process.env.PORT || 5000
 
-  app.use(express.json()); // Allows JSON data in the req.body
+    app.use(express.json()) // Allows JSON data in the req.body
 
-  // ✅ API Routes
-  app.use("/api/products", productRoutes);
+    // ✅ API Routes
+    app.use('/api/products', productRoutes)
 
-  // ✅ Start server only after DB connection is established
-  app.listen(PORT, () => {
-    console.log(`✅ Server started at http://localhost:${PORT}`);
-  });
-}).catch((error) => {
-  console.error("❌ Database connection failed:", error);
-  process.exit(1); // Stop server if DB fails to connect
-});
+    // ✅ Start server only after DB connection is established
+    app.listen(PORT, () => {
+      console.log(`✅ Server started at http://localhost:${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.error('❌ Database connection failed:', error)
+    process.exit(1) // Stop server if DB fails to connect
+  })
